923348a3a025039b2914b47904fe37cb47024f14
mishra-git <35693012+mishra-git@users.noreply.github.com>
2024-11-24 11:55:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,12 @@ session = cnx.session()
 #session = get_active_session()
 st.write("""Choose thse fruits from Database table""")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# Convert the snopark data frame to the pandas dataframe so we can use the loc function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect('choose up to 5 ingredients:',my_dataframe,max_selections=5)
