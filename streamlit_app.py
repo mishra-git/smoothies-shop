e35d410f2459573eba62160886c307c104ef7e7b
mishra-git <35693012+mishra-git@users.noreply.github.com>
2024-11-24 13:00:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the snopark data frame to the pandas dataframe so we can use the loc function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect('choose up to 5 ingredients:',my_dataframe,max_selections=5)
 #ingredients_string = ''
